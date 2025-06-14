de84293f3d1fdcec3faa810916f24cb345567fe1
Anthony Frank <afrank84@yahoo.com>
2025-06-14 09:00:07 -0400

    docusaurus.config.js - added dropdown menu to feedback

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,8 +91,21 @@ const config = {
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             label: 'Feedback',
-            to: '/feedback', // OR use `href: 'https://...'` to link externally
             position: 'left',
+            items: [
+              {
+                label: 'Submit Request',
+                to: '/feedback', // local page with embedded Google Form
+              },
+              {
+                label: 'Contact Me',
+                href: '#',
+              },
+              {
+                label: 'Report a Bug',
+                href: '#',
+              },
+            ],
           },
           {
             href: 'https://github.com/afrank84/frankndok/tree/gh_pages',
